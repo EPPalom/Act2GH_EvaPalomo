cec69d450cf252954ceccbdd0731496d222f8be0
EPPalom <115471500+EPPalom@users.noreply.github.com>
2022-11-14 17:02:55 +0100

    Añado ArrayNumeros.java

---
+++ ArrayNumeros.java
@@ -0,0 +1,14 @@
+
+public class NumeroVecesArray {
+
+  public static void main(String[] args) {
+    
+    int[] numeros = new int[20];
+
+    for (int i = 1; i <= 20; i++) {
+      numeros[i] = (int)(Math.random()*10+1);      
+    }    
+
+  }
+
+}
